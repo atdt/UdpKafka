21bcc59adc0fc705ebad07af0a94c258a57e157d
Ori Livneh <ori@wikimedia.org>
2013-01-15 12:51:26 -0800

    Handle more errors; improve docs.

--- src/UdpKafka.java
+++ src/UdpKafka.java
@@ -1,4 +1,4 @@
-/*
+/**
  *  _   _     _       _  __      __ _
  * | | | | __| |_ __ | |/ /__ _ / _| | ____ _
  * | | | |/ _` | '_ \| ' // _` | |_| |/ / _` |
@@ -6,8 +6,19 @@
  *  \___/ \__,_| .__/|_|\_\__,_|_| |_|\_\__,_|
  *           |_|
  *
+ * Route udplog data from UDP multicast to Kafka.
+ *
+ * Workload is balanced by bucketing messages on the sequence ID value
+ * and maintaining a separate instance for each worker. The sequence ID
+ * is hashed to account for any periodicity in the data stream.
+ *
+ * Configuration is read from 'udpkafka.properties' by default. An alt.
+ * configuration file can be specified as the first command-line argument.
+ *
  * @author Ori Livneh <ori@wikimedia.org>
+ * @version %I%, %G%
  */
+
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
@@ -27,18 +38,22 @@ public class UdpKafka {
     private static String topic;
     private static String udpGroup;
     private static int udpPort;
+    private static int udpNumWorkers;
     private static int udpWorkerId;
-    private static int udpWorkers;
-
+    private static MulticastSocket sock;
 
     public static void main(String args[]) {
+        // Allow config file to be specified as the first command-line
+        // argument. Default to "udpkafka.properties" if not specified.
         String configFile = args.length == 0
-                ? "udp2kafka.properties"
+                ? "udpkafka.properties"
                 : args[0];
         try {
             props.load(new FileInputStream(configFile));
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.format("Config file \"%s\" is missing or invalid.%n", configFile);
+            e.printStackTrace(System.err);
+            System.exit(1);
         }
         initConfig();
 
@@ -46,14 +61,20 @@ public class UdpKafka {
         Producer<String, String> producer = new Producer<String, String>(config);
 
         try {
-            MulticastSocket sock = new MulticastSocket(null);
+            sock = new MulticastSocket(null);
             sock.setReuseAddress(true);
             sock.bind(new InetSocketAddress(udpPort));
             sock.joinGroup(InetAddress.getByName(udpGroup));
+        } catch (IOException e) {
+            System.err.format("Failed to bind/join %s:%d.%n", udpGroup, udpPort);
+            e.printStackTrace(System.err);
+            System.exit(1);
+        }
 
-            byte[] buf = new byte[65536];
-            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+        byte[] buf = new byte[65536];  // Same as Udp2LogConfig::BLOCK_SIZE.
+        DatagramPacket packet = new DatagramPacket(buf, buf.length);
 
+        try {
             while (true) {
                 sock.receive(packet);
                 String msg = new String(buf, 0, packet.getLength());
@@ -63,34 +84,50 @@ public class UdpKafka {
                 packet.setLength(buf.length);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Died in main receive/send loop.");
+            e.printStackTrace(System.err);
+        } finally {
+            sock.close();
+            producer.close();
         }
-
     }
+
 
     /**
      * Load configuration values.
      */
     private static void initConfig() {
-        topic = props.getProperty("topic");
-        udpGroup = props.getProperty("udplog.group");
-        udpPort = Integer.parseInt(props.getProperty("udplog.port"));
-        udpWorkerId = Integer.parseInt(props.getProperty("udplog.id"));
-        udpWorkers = Integer.parseInt(props.getProperty("udplog.workers"));
+        try {
+            topic = props.getProperty("topic");
+            udpGroup = props.getProperty("udp.group");
+            udpPort = Integer.parseInt(props.getProperty("udp.port"));
+            udpWorkerId = Integer.parseInt(props.getProperty("udp.workerId"));
+            udpNumWorkers = Integer.parseInt(props.getProperty("udp.numWorkers"));
+        } catch (RuntimeException e) {
+            System.err.println("Invalid configuration.%n");
+            e.printStackTrace(System.err);
+            System.exit(1);
+        }
     }
 
+
     /**
-     * Determine if log line should be handled by this worker.
+     * @param msg Udplog log line.
+     * @return Whether log line should be handled by this worker.
      */
     private static boolean shouldHandle(String msg) {
         long seqId = getSeqId(msg);
-        return seqId != -1 && consistentHash(seqId, udpWorkers) == udpWorkerId;
+        return seqId != -1 && consistentHash(seqId, udpNumWorkers) == udpWorkerId;
     }
 
+
     /**
-     * Extract a long sequence ID from udplog log line.
+     * Extract a long sequence ID from a udplog log line.
      * The sequence ID is assumed to be a long integer in the second
      * field of a set of space-separated fields: blah 123 blah blah ..
+     *
+     * @param msg Udplog log line.
+     * @return Sequence ID or -1 if not found.
      */
     private static long getSeqId(String msg) {
         int start = msg.indexOf(' ');
@@ -110,10 +147,13 @@ public class UdpKafka {
         }
     }
 
+
     /**
      * Assigns to input a bucket in the range [0, buckets].
-     * Adapted from Guava; Copyright (C) 2011 The Guava Authors
+     * Adapted from Guava; Copyright (C) 2011 The Guava Authors.
      * Licensed under the Apache License, version 2.0.
+     *
+     * @return Bucket assignment.
      */
     private static int consistentHash(long input, int buckets) {
         long h = input;
